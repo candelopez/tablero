8472b5dcbdc26dd4fe53c4407527298ec8883099
candelopez <candelopezcarlos@gmail.com>
2017-07-22 15:14:04 -0500

    se da por terminado el código. Faltó optimizar el código usando jquery para cambiar los estilos del archivo css. Además faltó centrar el div del tablero y colocar el botón en la parte de abajo

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 
-
 $(document).ready(function(){
-	for (var j = 0; j < 5; j++) {
-		for (var i = 0; i < 5; i++) {
-			$("body").append("<div></div>");
-		}
-	$("div:last-child").attr("style","clear: left");
-}
+
+
+$("#boton").click(function(){
+	var lados=window.prompt("Numero de lados",20);
+	var pixLados=500/lados;
+	creaDivs(lados,pixLados);
+	coloreaDivs();
+	});
 });
+
+function creaDivs(lados,numLados){
+	$("#tablero div").remove();
+
+	$("#tablero").append("<div></div>")
+
+	for (var j = 0; j < lados; j++) {
+		for (var i = 0; i < lados; i++) {
+			$("#tablero").append("<div></div>");
+				}
+		$("div:last-child").attr("style","clear: left");
+	}
+
+	$("#tablero div:last-child").remove();
+
+	$('#tablero div').css('width', numLados+'px');
+	$('#tablero div').css('height', numLados+'px');
+}
+
+function coloreaDivs(){
+	$("#tablero div").hover(function(){
+	$(this).css('background-color','red');
+	});
+}
